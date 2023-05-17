3dcaee28c939a1419ea17d5532f42896bf49d123
MK0503 <ki8418knut@gmail.com>
2023-05-17 18:18:20 +0900

    Refracting UI

--- app/src/main/java/com/example/mocap01/Home01.java
+++ app/src/main/java/com/example/mocap01/Home01.java
@@ -3,6 +3,7 @@ package com.example.mocap01;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +12,14 @@ import android.view.ViewGroup;
 
 public class Home01 extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.home, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home, container, false);
+        return view;
     }
 }
