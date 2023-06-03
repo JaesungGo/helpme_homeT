07f7bd9939ac78e35c429bdac26f1492ba7d66e5
JaesungGo <beeniy@naver.com>
2023-06-04 01:13:44 +0900

    Add Other Exercise // Fix CalendarView ( Record01 )

--- app/src/main/java/com/example/mocap01/Record.java
+++ app/src/main/java/com/example/mocap01/Record.java
@@ -3,7 +3,9 @@ package com.example.mocap01;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListView;
@@ -36,7 +38,6 @@ public class Record extends AppCompatActivity {
     List<Object> Array = new ArrayList<Object>();
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,48 +45,41 @@ public class Record extends AppCompatActivity {
 
         today = findViewById(R.id.today);
         calendarView = findViewById(R.id.calendarView);
-        listView2 = (ListView) findViewById(R.id.listView2);
+//        listView2 = findViewById(R.id.listView2);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>());
+        mFirebaseAuth = FirebaseAuth.getInstance(); // 파이어베이스 인증 객체 초기화
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<>());
         listView2.setAdapter(adapter);
 
-
-        //날짜변환
-        DateFormat formatter = new SimpleDateFormat("yyyy년MM월dd일");
+        @SuppressLint("SimpleDateFormat") DateFormat formatter = new SimpleDateFormat("yyyy년MM월dd일");
         Date date = new Date(calendarView.getDate());
         today.setText(formatter.format(date));
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String day;
-                day = year + "년" + (month+1) + "월" + dayOfMonth + "일";
-                today.setText(day);
-            }
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String day = year + "년" + (month + 1) + "월" + dayOfMonth + "일";
+            today.setText(day);
+            Log.d("Record", "Today: " + year);
         });
 
-        mDatabaseRef.child(mFirebaseAuth.getUid()).child("Check2").child("Squat").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                adapter.clear();
-                for (DataSnapshot messageData : dataSnapshot.getChildren()) {
-                    String msg2 = messageData.getValue().toString();
-                    Array.add(msg2);
-                    adapter.add(msg2);
-                    // child 내에 있는 데이터만큼 반복합니다.
-                }
-                adapter.notifyDataSetChanged();
-                listView2.setSelection(adapter.getCount() - 1);
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
+//        mDatabaseRef.child(mFirebaseAuth.getUid()).child("Check2").child("Squat").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                adapter.clear();
+//                for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+//                    String msg2 = messageData.getValue().toString();
+//                    Array.add(msg2);
+//                    adapter.add(msg2);
+//                    // child 내에 있는 데이터만큼 반복합니다.
+//                }
+//                adapter.notifyDataSetChanged();
+//                listView2.setSelection(adapter.getCount() - 1);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
     }
-
 }
-
-
--- app/src/main/java/com/example/mocap01/Record01.java
+++ app/src/main/java/com/example/mocap01/Record01.java
@@ -1,22 +1,47 @@
 package com.example.mocap01;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Record01 extends Fragment {
 
-
+    private CalendarView calendarView;
+    private TextView today;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.record, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.record, container, false);
+
+        today = rootView.findViewById(R.id.today);
+        calendarView = rootView.findViewById(R.id.calendarView);
+
+        // 날짜 변환
+        DateFormat formatter = new SimpleDateFormat("yyyy년MM월dd일");
+        Date date = new Date(calendarView.getDate());
+        today.setText(formatter.format(date));
+
+        // 캘린더뷰 클릭 이벤트 처리
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String day = year + "년" + (month + 1) + "월" + dayOfMonth + "일";
+                today.setText(day);
+            }
+        });
+
+        return rootView;
     }
 }
